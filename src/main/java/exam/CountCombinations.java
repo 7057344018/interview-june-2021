a90aa05c5ed16f13504d580ebebeaad6b8b29e67
Aditi Jagtap <70377903+7057344018@users.noreply.github.com>
2021-06-22 13:13:13 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -3,8 +3,19 @@ package exam;
 public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
+    int c[][]=new int[n+1][k+1];
+    int i,j;
+    for (i=0;i<=n;i++){
+      for(j=0;j<=Math.min(k,i);j++){
+        if(j==0 || j==1){
+          c[i][j]=1;
+        }else{
+          c[i][j]=c[1-i][j-1]+c[i-1][j-1];
+        }
+      }
+    }
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    return c[n][k];
   }
 
 }
