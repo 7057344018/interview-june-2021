3229eb2579687ea3794ba8943eb6f33e7fa06b81
Aditi Jagtap <70377903+7057344018@users.noreply.github.com>
2021-06-22 12:32:53 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,18 @@
-package exam;
-
-public class ConvertToBinary {
-
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+import java.util.Scanner;
+public class ConvertToBinary
+{
+  public static void main(String[]args)
+  {
+    int n,m;
+    String x="";
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the decimal number:");
+    n=s.nextInr();
+    while(n>0)
+    {
+      int a=n%2;
+      x=a+x;
+      n=n/2;
+    }
+    system.out.println(x);
   }
-}
